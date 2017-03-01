f585e9bd9681ad7b366308d8089ef61c094e9bbe
DesankaTodic <desankatodic@yahoo.com>
2017-03-01 18:13:54 +0100

    Create Restaurant.java

---
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/domain/Restaurant.java
@@ -0,0 +1,116 @@
+package rs.ac.uns.ftn.informatika.jpa.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+public class Restaurant implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+    @GeneratedValue
+	@Column(unique=true, nullable=false)
+	private Long id;
+	@Column(nullable=false)
+	private String name;
+	@Column(name="type_id", nullable=false)
+	private String type;
+	@Column( nullable=true)
+	private String address;
+	@Column(nullable=true)
+	private String contact;
+	@Column(nullable=true)
+	private float width;
+	@Column(nullable=true)
+	private float height;
+	
+	public Restaurant(){
+		
+	}
+	
+	public Restaurant(String name, String type, String address, String contact, float width, float height){
+		this.name = name;
+		this.type = type;
+		this.address = address;
+		this.contact = contact;
+		this.width = width;
+		this.height = height;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+}
